e83bc339019647d60ac2d9e1f0604076c4a512d2
faraz khan <farazkhana1600@gmail.com>
2026-02-09 20:01:00 +0500

    adding

--- backend/src/main.py
+++ backend/src/main.py
@@ -46,7 +46,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://hackathon-2-fullstack-todo.vercel.app/",
+        "https://fullstack-todo-app-psi.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
